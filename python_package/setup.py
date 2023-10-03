16079dba8883d9c65b18e2bc2e036e16aef9911e
Amit Gupta <amit_@outlook.in>
2023-10-03 16:16:51 -0500

    SOAP cleanup and added python package

--- python_package/setup.py
+++ python_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="libdescriptor_dev",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     package_data={
         'libdescriptor': [
